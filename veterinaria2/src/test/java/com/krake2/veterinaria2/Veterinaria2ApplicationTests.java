890103e64b3634a74ea43ee87eb6a39bd4a5d1ae
Tin <136649867+martin1906s@users.noreply.github.com>
2025-12-16 15:14:36 -0500

    reto3

---
+++ veterinaria2/src/test/java/com/krake2/veterinaria2/Veterinaria2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krake2.veterinaria2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Veterinaria2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

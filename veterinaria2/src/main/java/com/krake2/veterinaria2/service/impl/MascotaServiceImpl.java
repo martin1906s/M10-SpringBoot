af0f0dbfd96d5f4103e1ba12b292c0e1d0c6d30b
Tin <136649867+martin1906s@users.noreply.github.com>
2025-12-17 14:34:53 -0500

    reto12

---
+++ veterinaria2/src/main/java/com/krake2/veterinaria2/service/impl/MascotaServiceImpl.java
@@ -0,0 +1,63 @@
+package com.krake2.veterinaria2.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.krake2.veterinaria2.entity.Mascota;
+import com.krake2.veterinaria2.repository.MascotaRepository;
+import com.krake2.veterinaria2.service.MascotaService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+
+@Service
+@RequiredArgsConstructor
+public class MascotaServiceImpl implements MascotaService {
+
+    private MascotaRepository mascotaRepository;
+
+    @Override
+    public Mascota registrarMascota(Mascota mascota) {
+        return mascotaRepository.save(mascota);
+    }
+
+    @Override
+    public List<Mascota> listarMascotas() {
+        return mascotaRepository.findAll();
+    }
+
+    @Override
+    public Optional<Mascota> buscarPorNombre(String nombre) {
+        return mascotaRepository.findByNombre(nombre);
+    }
+
+    @Override
+    public Optional<Mascota> buscarPorId(Long id) {
+        return mascotaRepository.findById(id);
+    }
+
+    @Override
+    @SneakyThrows
+    public Mascota actualizarMascota(Long id, Mascota mascota) {
+        Mascota mascotaExistente = mascotaRepository.findById(id).orElseThrow(() -> new Exception("Producto con id " + id + " no encontrado"));
+        mascotaExistente.setNombre(mascota.getNombre());
+        mascotaExistente.setEspecie(mascota.getEspecie());
+        mascotaExistente.setEdad(mascota.getEdad());
+        mascotaExistente.setNombreDueno(mascota.getNombreDueno());
+        mascotaExistente.setFechaRegistro(mascota.getFechaRegistro());
+
+        Mascota mascotaActualizada = mascotaRepository.save(mascotaExistente);
+        return mascotaActualizada;
+    }
+
+    @Override
+    @SneakyThrows
+    public void eliminarMascota(Long id) {
+        mascotaRepository.findById(id).orElseThrow(() -> new Exception("Producto con id " + id + " no encontrado"));
+        mascotaRepository.deleteById(id);
+
+    }
+
+}

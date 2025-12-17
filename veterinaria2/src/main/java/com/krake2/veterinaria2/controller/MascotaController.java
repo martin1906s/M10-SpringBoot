db146f6cb5ecaf0780452312d3ae676823e038c8
Tin <136649867+martin1906s@users.noreply.github.com>
2025-12-17 12:05:56 -0500

    reto 8

--- veterinaria2/src/main/java/com/krake2/veterinaria2/controller/MascotaController.java
+++ veterinaria2/src/main/java/com/krake2/veterinaria2/controller/MascotaController.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.krake2.veterinaria2.entity.Mascota;
+
 
 @RestController
 @RequestMapping("/api/mascotas")
@@ -36,4 +40,15 @@ public class MascotaController {
         return mascota.orElse(null);
     }
 
+    @PostMapping
+    public Mascota createMascota(@RequestBody Mascota mascota){
+        mascotas.add(mascota);
+        return mascota;
+    }
+    
+    @DeleteMapping("/{id}")
+    public String deleteMascota(@PathVariable int id){
+        mascotas.removeIf(m->m.getId()==id);
+        return "Mascota eliminada";
+    }
 }
